1a62b80936067910dfb3b5134051c2a51ac1a19e
kinematicbody3 <78506010+kinematicbody3@users.noreply.github.com>
2021-02-04 18:10:38 +0300

    Add files via upload

---
+++ sprites.pas
@@ -0,0 +1,23 @@
+﻿unit sprites;
+
+interface
+uses
+  GraphABC,ABCObjects;
+
+
+
+//////----------------****************************************************---------------------------////
+procedure Sprite(x,y:integer;  texture: picture);
+
+//////----------------****************************************************---------------------------////
+
+implementation
+uses Main;
+
+procedure Sprite(x,y:integer;  texture: picture);
+begin
+texture.Draw(X,Y);
+end;
+
+
+end.

dea41579d3bb6e6a48b0de766b5e4f7e4e11f53a
kinematicbody3 <78506010+kinematicbody3@users.noreply.github.com>
2021-02-05 18:57:55 +0300

    Add files via upload

---
+++ resurce.pas
@@ -0,0 +1,54 @@
+﻿unit resurce;
+
+interface
+uses
+  GraphABC,ABCObjects,sprites;
+
+Type
+  Player = record
+    x,y,sx,sy:integer;
+  end;
+
+//////----------------****************************************************---------------------------////
+procedure Res_Load();
+procedure MouseMove(x,y,mb: integer);
+procedure KeyDown(Key:integer);
+//////----------------****************************************************---------------------------////
+
+implementation
+uses Main;
+{procedure MouseDown(x,y,mb: integer);
+begin
+  MoveTo(x,y);
+end;}
+  
+  procedure KeyDown(Key:integer);
+  begin
+    case Key of
+        VK_Left:  p.x-=5;
+        VK_Right: p.x+=5;
+        VK_Up:    p.y-=5;
+        VK_Down:  p.y+=5;
+    end;   
+  end; 
+
+
+
+procedure MouseMove(x,y,mb: integer);
+begin
+  //if mb=1 then LineTo(x,y);
+  m.x:=x;
+  m.y:=y;
+end;
+
+procedure Res_Load();
+begin
+    box:=Sprite_load('data\box.png');
+    fon:=Sprite_load('data\fon.png');
+    ast:=Sprite_load('data\asteroid.png');    
+    
+    
+end;
+
+
+end.

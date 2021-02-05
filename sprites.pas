f17cdf8343507ed3c7277119694d683d3cc6c6b2
kinematicbody3 <78506010+kinematicbody3@users.noreply.github.com>
2021-02-05 18:58:35 +0300

    Add files via upload

---
+++ sprites.pas
@@ -0,0 +1,30 @@
+﻿unit sprites;
+
+interface
+uses
+  GraphABC,ABCObjects;
+
+
+
+//////----------------****************************************************---------------------------////
+
+function Sprite_Load(put:string):picture;
+
+//////----------------****************************************************---------------------------////
+
+implementation
+uses Main,resurce;
+
+
+
+function Sprite_Load(put:string):picture;
+var
+  p:picture;
+begin
+    p:=Picture.Create(put);
+    p.Load(put);
+    result:=p;
+end;
+
+
+end.
